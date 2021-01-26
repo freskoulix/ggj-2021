30ba386d26a37b2b5bd2887aefcd64f4c93c49db
Christos Nianiakas <cn@wappier.com>
2021-01-26 21:54:36 +0200

    Added basic 3rd person movement

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,24 +1,41 @@
 extends Node
 
+const KEYBOARD_SENS = 0.2
+const MOUSE_SENS = 0.002
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var scene = get_tree().get_current_scene()
+onready var character = scene.get_node('Character')
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	keyboard_handler()
+
+func _input(event): 
+	if event is InputEventMouseMotion:
+		mouse_handler(event)
+	
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
+func keyboard_handler():
 	if Input.is_key_pressed(KEY_W):
-		pass
+		var pos = Vector3(0, 0, -KEYBOARD_SENS)
+		character.translate(pos)
 	elif Input.is_key_pressed(KEY_S):
-		pass
+		var pos = Vector3(0, 0, KEYBOARD_SENS)
+		character.translate(pos)
 
 	if Input.is_key_pressed(KEY_A):
-		pass
+		var pos = Vector3(-KEYBOARD_SENS, 0, 0)
+		character.translate(pos)
 	elif Input.is_key_pressed(KEY_D):
-		pass
+		var pos = Vector3(+KEYBOARD_SENS, 0, 0)
+		character.translate(pos)
+
+func mouse_handler(event):
+	var move = event.relative * MOUSE_SENS
+
+	character.rotate_y(-move.x)
+	character.orthonormalize()
