cb868643c62ae8e81d9f40ce31721104da9cfdd9
Christos Nianiakas <cnianiak@gmail.com>
2021-01-30 21:16:51 +0200

    Added animation loop transition for Jump

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -144,9 +144,13 @@ func anim_handler(on_air, in_motion):
 			return
 		"jump":
 			if not on_air and not in_motion:
+				animation_player.stop(true)
+				animation_player.clear_queue()
 				state_machine.travel("Idle")
 				characterState = "idle"
 			elif not on_air and in_motion:
+				animation_player.stop(true)
+				animation_player.clear_queue()
 				state_machine.travel("Running")
 				characterState = "running"
 			return
