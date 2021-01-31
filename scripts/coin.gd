8c98a6923375351611ed4ad4c5bf420c3f9d674b
Achilleas John Yfantis <achilleasein@gmail.com>
2021-01-31 14:19:33 +0200

    added coin dissapear

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,14 @@
 extends Area
 
-
 func _ready():
 	pass
 
 func _physics_process(delta):
 	rotate_y(deg2rad(3))
+
+func _on_coin_body_entered(body):
+	if body.name == "Player":
+		$Timer.start()
+
+func _on_Timer_timeout():
+	queue_free()
